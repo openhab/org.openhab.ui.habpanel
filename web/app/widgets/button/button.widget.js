0caa3ee55d12a33b597bbdb0dd848718de816833
Lucky <luckymallari@gmail.com>
2018-01-02 02:39:53 -0800

    Button widget: Value display and external URL navigation
    This adds the ability to navigate to an external URL, optionally in a new tab or new window when clicking a Button widget. It can now optionally display the value of an item's state, with different colors depending on an active/inactive status.
    
    The widget's settings dialog's tab order is changed and similar options are now grouped together.
    
    Templates cleanup.
    
    Remove openHAB 2 mentions in the "use server-provided format" options (Dummy, Button widgets).
    
    Also-by: Yannick Schaus <habpanel@schaus.net>
    Signed-off-by: Lucky Mallari <luckymallari@gmail.toDotCom>

--- web/app/widgets/button/button.widget.js
+++ web/app/widgets/button/button.widget.js
@@ -1,11 +1,11 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('app.widgets')
         .directive('widgetButton', widgetButton)
         .controller('WidgetSettingsCtrl-button', WidgetSettingsCtrlButton)
-        .config(function (WidgetsProvider) { 
+        .config(function (WidgetsProvider) {
             WidgetsProvider.$get().registerType({
                 type: 'button',
                 displayName: 'Button',
@@ -32,29 +32,65 @@
             }
         };
         return directive;
-        
+
         function link(scope, element, attrs) {
             element[0].parentElement.parentElement.className += " activefeedback";
         }
     }
-    ButtonController.$inject = ['$rootScope', '$scope', '$location', 'OHService'];
-    function ButtonController ($rootScope, $scope, $location, OHService) {
+    ButtonController.$inject = ['$rootScope', '$scope', '$location', 'OHService', '$window', 'themeValueFilter'];
+    function ButtonController($rootScope, $scope, $location, OHService, $window, themeValueFilter) {
         var vm = this;
         this.widget = this.ngModel;
-        
+
         vm.background = this.widget.background;
         vm.foreground = this.widget.foreground;
         vm.font_size = this.widget.font_size;
 
         function updateValue() {
             vm.value = OHService.getItem(vm.widget.item).state;
-            if (vm.value === vm.widget.command) {
+            if (vm.value === vm.widget.command
+                || (vm.widget.action_type === 'navigate' && vm.widget.show_item_value && vm.value == vm.widget.navigate_active_state)) {
                 vm.background = vm.widget.background_active;
                 vm.foreground = vm.widget.foreground_active;
+                if (vm.widget.show_item_value) {
+                    vm.value_color = themeValueFilter(vm.widget.value_color_active, 'primary-color');
+                }
             } else {
                 vm.background = vm.widget.background;
                 vm.foreground = vm.widget.foreground;
+                if (vm.widget.show_item_value) {
+                    vm.value_color = themeValueFilter(vm.widget.value_color, 'primary-color');
+                }
             }
+        }
+
+        function onNavigate() {
+            if (vm.widget.navigate_dashboard) {
+                $location.url('/view/' + vm.widget.navigate_dashboard);
+                return;
+            }
+
+            if (!vm.widget.navigate_url)
+                return;
+
+            switch (vm.widget.navigate_target || 'self') {
+                case 'new_tab':
+                    var w = $window.open(vm.widget.navigate_url);
+                    w && (w.opener = null);
+                    break;
+
+                case 'new_window':
+                    var w = $window.open(vm.widget.navigate_url, "_blank", "resizable=1", true);
+                    w && (w.opener = null);
+                    break;
+
+                default:
+                case 'self': {
+                    $window.location.href = vm.widget.navigate_url;
+                    break;
+                }
+            }
+
         }
 
         OHService.onUpdate($scope, vm.widget.item, function () {
@@ -64,10 +100,9 @@
         vm.sendCommand = function () {
             switch (vm.widget.action_type) {
                 case "navigate":
-                    if (vm.widget.navigate_dashboard) {
-                        $location.url('/view/' + vm.widget.navigate_dashboard);
-                    }
+                    onNavigate();
                     break;
+
                 case "toggle":
                     if (vm.widget.command && vm.widget.command_alt) {
                         if (vm.value === vm.widget.command) {
@@ -103,47 +138,96 @@
             action_type: widget.action_type || 'command',
             command: widget.command,
             command_alt: widget.command_alt,
-            navigate_dashboard: widget.navigate_dashboard,
             background: widget.background,
             foreground: widget.foreground,
+            value_color: widget.value_color,
             font_size: widget.font_size,
             background_active: widget.background_active,
             foreground_active: widget.foreground_active,
+            value_color_active: widget.value_color_active,
             backdrop_iconset: widget.backdrop_iconset,
             backdrop_icon: widget.backdrop_icon,
-            backdrop_center : widget.backdrop_center,
+            backdrop_center: widget.backdrop_center,
             iconset: widget.iconset,
             icon: widget.icon,
             icon_size: widget.icon_size,
             icon_nolinebreak: widget.icon_nolinebreak,
-            icon_replacestext: widget.icon_replacestext
+            icon_replacestext: widget.icon_replacestext,
+            navigate_type: (widget.navigate_url) ? 'url' : 'dashboard',
+            navigate_url: widget.navigate_url,
+            navigate_dashboard: widget.navigate_dashboard,
+            navigate_target: widget.navigate_target || 'self',
+            navigate_active_state: widget.navigate_active_state,
+            show_item_value: widget.show_item_value,
+            value_unit: widget.value_unit,
+            value_font_size: widget.value_font_size,
+            value_format: widget.value_format,
+            value_useserverformat: widget.value_useserverformat,
+            value_nolinebreak: widget.value_nolinebreak
         };
 
-        $scope.dismiss = function() {
+        $scope.dismiss = function () {
             $modalInstance.dismiss();
         };
 
-        $scope.remove = function() {
+        $scope.remove = function () {
             $scope.dashboard.widgets.splice($scope.dashboard.widgets.indexOf(widget), 1);
             $modalInstance.close();
         };
 
-        $scope.submit = function() {
+        $scope.submit = function () {
             angular.extend(widget, $scope.form);
             switch (widget.action_type) {
                 case "navigate":
-                    delete widget.item;
                     delete widget.command;
                     delete widget.command_alt;
+
+                    if ($scope.form.navigate_type === 'dashboard') {
+                        delete widget.navigate_url;
+                        delete widget.navigate_target;
+                    } else {
+                        delete widget.navigate_dashboard;
+                    }
+
+                    if (!$scope.form.show_item_value) {
+                        delete widget.item;
+                        delete widget.navigate_active_state;
+                    } else {
+                        delete widget.background_active;
+                        delete widget.foreground_active;
+                        delete widget.value_color_active;
+                    }
+
                     break;
+
                 case "toggle":
+                    delete widget.navigate_url;
                     delete widget.navigate_dashboard;
+                    delete widget.navigate_type;
+                    delete widget.navigate_target;
+                    delete widget.navigate_active_state;
                     break;
+
                 default:
                     delete widget.command_alt;
+                    delete widget.navigate_url;
                     delete widget.navigate_dashboard;
+                    delete widget.navigate_type;
+                    delete widget.navigate_target;
+                    delete widget.navigate_active_state;
                     delete widget.action_type;
                     break;
+            }
+
+            if (!widget.show_item_value) {
+                delete widget.show_item_value;
+                delete widget.value_unit;
+                delete widget.value_font_size;
+                delete widget.value_format;
+                delete widget.value_useserverformat;
+                delete widget.value_nolinebreak;
+                delete widget.value_color;
+                delete widget.value_color_active;
             }
 
             $modalInstance.close(widget);
